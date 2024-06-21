cd33785ee1a0509c51d4b75fb5e32a6bab58f3d0
jhonrivera1100 <jhonrivera416@gmail.com>
2024-06-21 16:30:36 -0500

    diseño maquinas

--- client opalsa/src/pages/RegistroMantenimiento.jsx
+++ client opalsa/src/pages/RegistroMantenimiento.jsx
@@ -13,11 +13,19 @@ const RegistroMantenimiento = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 ">
+    <div className="min-h-screen bg-gray-100">
       <Navbar />
       <UserHeader />
-      <MantenimientoRegistro />
-      <CrearRecordatorio usuario={user} onRecordatorioCreado={handleRecordatorioCreado} />
+      <div className="flex justify-center items-start p-4 gap-4">
+        <div className="w-1/2 p-4 bg rounded-lg shadow-md">
+          <h2 className="text-xl font-bold mb-4">Registro de Mantenimiento</h2>
+          <MantenimientoRegistro />
+        </div>
+        <div className="w-1/2 p-4 bg-white rounded-lg shadow-md">
+          <h2 className="text-xl font-bold mb-4">Crear Recordatorio</h2>
+          <CrearRecordatorio usuario={user} onRecordatorioCreado={handleRecordatorioCreado} />
+        </div>
+      </div>
     </div>
   );
 };
